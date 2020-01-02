c154d401ad500684534137249279f8cab680faa5
Kleber Coelho <kcoelho@komputer.com.br>
2020-01-02 20:25:06 -0200

    Crud Empresa

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Empresa
 
 
@@ -16,3 +16,7 @@ class EmpresaCreate(CreateView):
         funcionario.empresa = obj
         funcionario.save()
         return HttpResponse('Ok')
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
